8cbabd43b1e74429290727f6a77bf7e5236b3fc4
Obed Andrade <obed@rumika.local>
2026-08-18 08:25:40 -0400

    change route of welcome

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
