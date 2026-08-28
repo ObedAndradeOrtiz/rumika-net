41a491b6f2403444d9abaaf905f7cf79bceece61
Obed Andrade <obed@rumika.local>
2026-08-28 09:27:50 -0400

    Compactar metricas de agenda

--- resources/views/livewire/clinic/agenda-manager.blade.php
+++ resources/views/livewire/clinic/agenda-manager.blade.php
@@ -48,15 +48,31 @@
         </div>
 
         <div class="rm-agenda-day-stats" aria-label="Resumen del dia">
-            <div>
+            <div class="is-scheduled">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2.3">
+                    <path d="M8 2v4M16 2v4M3 10h18" />
+                    <rect x="3" y="4" width="18" height="18" rx="3" />
+                </svg>
                 <span>Agendados</span>
                 <strong>{{ $agendaStats['scheduled'] }}</strong>
             </div>
-            <div>
+            <div class="is-attended">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2.4">
+                    <path d="M20 6 9 17l-5-5" />
+                </svg>
                 <span>Asistidos</span>
                 <strong>{{ $agendaStats['attended'] }}</strong>
             </div>
-            <div>
+            <div class="is-rescheduled">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2.3">
+                    <path d="M3 12a9 9 0 0 1 15.5-6.2L21 8" />
+                    <path d="M21 3v5h-5" />
+                    <path d="M21 12a9 9 0 0 1-15.5 6.2L3 16" />
+                    <path d="M3 21v-5h5" />
+                </svg>
                 <span>Reagendados</span>
                 <strong>{{ $agendaStats['rescheduled'] }}</strong>
             </div>
